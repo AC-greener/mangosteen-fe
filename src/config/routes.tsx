d1ef00a940ce9c199e484f0645e07283c764804e
tongtong <1053673440@qq.com>
2022-09-12 13:52:24 +0800

    feat：禁止在手机上缩放

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -5,11 +5,15 @@ import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
+  {
+    path: '/',
+    redirect: '/welcome'
+  },
   {
     path: '/welcome',
     component: Welcome,
     children: [
+      {path: '', redirect: '/welcome/1'},
       { path: '1', component: First, },
       { path: '2', component: Second, },
     ]
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -4,6 +4,7 @@ import s from './Welcome.module.scss'
 import logo from '../assets/icons/mangosteen.svg'
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    console.log(props, context)
     return () => <div class={s.wrapper}>
       <header>
         <img src={logo} />
